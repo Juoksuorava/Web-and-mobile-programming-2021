9e5d9c8ad0f5b34f9e418d258e0605263f812dc5
Lauri <lauriaslak@gmail.com>
2021-03-17 01:57:37 +0200

    Part 3 - Exercises 3.1 - 3.5 done - primitive backend for reminderapp established

--- Part 3/reminderapp/backend/index.js
+++ Part 3/reminderapp/backend/index.js
@@ -28,15 +28,15 @@ let reminders = [
       }
 ]
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
 })
 
-app.get('/reminders', (request, response) => {
+app.get('/api/reminders', (request, response) => {
     response.json(reminders)
 })
 
-app.get('/reminders/:id', (request, response) => {
+app.get('/api/reminders/:id', (request, response) => {
     const id = Number(request.params.id)
     const reminder = reminders.find(reminder => reminder.id === id)
     
@@ -47,18 +47,18 @@ app.get('/reminders/:id', (request, response) => {
     }
 })
 
-app.delete('/reminders/:id', (request, response) => {
+app.delete('/api/reminders/:id', (request, response) => {
     const id = Number(request.params.id)
     reminders = reminders.filter(reminder => reminder.id !== id)
     response.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = reminders.length > 0 ? reminders.map(r => r.id).sort((a, b) => a - b).reverse()[0] : 1
-    return maxId + 1
-}
+const generateId = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+  
 
-app.post('/reminders', (request, response) => {
+app.post('/api/reminders', (request, response) => {
     const body = request.body
 
     if (body.topic === undefined) {
@@ -69,10 +69,14 @@ app.post('/reminders', (request, response) => {
         return response.status(400).json({error: 'time missing'})
     }
 
+    if (reminders.some(reminder => reminder.topic === body.topic)) {
+        return response.status(400).json({error: 'topic must be unique'})
+    }
+
     const reminder = {
         topic: body.topic,
         time: body.time,
-        id: generateId()
+        id: generateId(0, 100 * 100)
     }
 
     reminders = reminders.concat(reminder)
