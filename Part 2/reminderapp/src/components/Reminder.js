b0f5052677e2ee8179ca0188c5801d8544ea4c79
Lauri <laorav@utu.fi>
2021-04-14 13:20:33 +0300

    Localized time for reminderapp

--- Part 2/reminderapp/src/components/Reminder.js
+++ Part 2/reminderapp/src/components/Reminder.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Reminder = (props) => {
     return (
       <li>
-        {props.topic} at {props.time} {" "}
+        {props.topic} at {new Date(props.time).toLocaleString()} {" "}
         <button onClick={() => props.deleteReminder(props.id)}>Delete</button>
       </li>
     )
